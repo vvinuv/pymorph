50f7e9fabfcc87f0a4b842d67e1125fe093c6221
vinu <vinu@2dd86497-8ce7-14b7-f524-3852097d2217>
2008-03-12 17:48:23 +0000

    redefine Run as the number run per rootname
    git-svn-id: https://pymorph.googlecode.com/svn@146 2dd86497-8ce7-14b7-f524-3852097d2217

--- serial_pipeline/trunk/pymorph/utilities.py
+++ serial_pipeline/trunk/pymorph/utilities.py
@@ -31,26 +31,35 @@ def WriteDb(ParamValues):
         DictParamWithValue[DBparam[0]] = DBparam[1]
         AllParams.append(DBparam[0])
     Total_Run = 1
-    try:
-        cmd = "SELECT Name from %s" % tbl
-        cursor.execute(cmd)
-        rows = cursor.fetchall()
-        for row in rows:
-            if str(row[0]) == gal_id:
-                Total_Run += 1
-    except:
-        pass
     Run = 1
     try:
-        cmd = "SELECT Name, ObsID from %s" % tbl
+        cmd = "SELECT Name, rootname from %s" % tbl
         cursor.execute(cmd)
         rows = cursor.fetchall()
         for row in rows:
+            print row
+            if str(row[0]) == gal_id:
+                Total_Run += 1
             if str(row[0]) == gal_id and \
-               str(row[1]) == DictParamWithValue['ObsID']:
+               str(row[1]) == c.rootname:
                 Run += 1
     except:
         pass
+#    Run = 1
+#    try:
+#        cmd = "SELECT Name, rootname from %s" % tbl
+#        print '1'
+#        cursor.execute(cmd)
+#        print '2'
+#        rows = cursor.fetchall()
+#        print '3'
+#        for row in rows:
+#            print row[0], row[1]
+#            if str(row[0]) == gal_id and \
+#               str(row[1]) == DictParamWithValue['rootname']:
+#                Run += 1
+#    except:
+#        pass
     x=datetime.date.today()
     yyyy, mm, dd = x.year, x.month, x.day
     DaTe = str(yyyy) + '.' + str(mm) + str(dd)
@@ -58,14 +67,17 @@ def WriteDb(ParamValues):
     AllParams.append('Version')
     AllParams.append('Filter')
     AllParams.append('Total_Run')
+    AllParams.append('rootname')
     DictParamWithType1['Date'] = 'varchar(50)' 
     DictParamWithType1['Version'] = 'float'
     DictParamWithType1['Filter'] = 'varchar(500)'
     DictParamWithType1['Total_Run'] = 'int'
+    DictParamWithType1['rootname'] = 'varchar(500)'
     DictParamWithValue['Date'] = DaTe
     DictParamWithValue['Version'] = c.VERSION
     DictParamWithValue['Filter'] = c.FILTER
     DictParamWithValue['Total_Run'] = Total_Run
+    DictParamWithValue['rootname'] = c.rootname
     if c.decompose:
         DictParamWithType2 = {'Name':'varchar(500)', 'ra':'float', \
                         'dec_':'float',\
